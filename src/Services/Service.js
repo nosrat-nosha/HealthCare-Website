b2b4f440c188817e73f9f76a662a11a0c607632c
Nosrat Nosha <nosrat.nosha@gmail.com>
2021-10-19 23:55:27 +0600

    commited

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -1,59 +1,38 @@
 import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 
 const Service = () => {
 	// useState for services
-	const [services, setServices] = useState([]);
+
 	// useState for services
+	const [services, setServices] = useState([]);
+	const { serviceKey } = useParams();
 	useEffect(() => {
 		fetch("/services.JSON")
 			.then((res) => res.json())
 			.then((data) => setServices(data));
 	}, []);
-	const { serviceKey } = useParams();
-	return (
-		<div>
-			<section>
-				<div className="row mt-5">
-					<div className="col-lg-4 text-center mx-auto">
-						<h5 className="text-success  ">Our Services ={serviceKey}</h5>
-						<h2>Our Health Care Services</h2>
-						<p className="text-secondary">
-							Your health is our first priority. We always try to give you our
-							best services
-						</p>
-					</div>
-					<hr />
-				</div>
-				<div className="container row row-cols-1 row-cols-md-1 gy-5 mx-auto mt-1">
-					{services?.map((service) => (
-						<div className="col">
-							<div className="card shadow service-card h-100">
-								<img
-									src={service.img}
-									className="card-img-top  mx-auto mt-3 w-25"
-									alt="..."
-								/>
 
-								<div className="card-body mt-3">
-									<h5 className="card-title text-success">{service.name}</h5>
-									<p className="card-text">
-										<h4>What is {service.name} ?</h4> <br />
-										{service.description}
-									</p>
-								</div>
-								<button className="btn w-50 btn-service  m-3">
-									<Link className="button-service" to="/home">
-										{" "}
-										SEE MORE <i class="fas fa-arrow-right ms-2"></i>
-									</Link>{" "}
-								</button>
-							</div>
-						</div>
-					))}
-				</div>
-			</section>
+	const selected = services.find((service) => service.key === serviceKey);
+
+	return (
+		<div className="">
+			{/* <h1>{ selected?.name }</h1> */}
+			<Card className="mx-auto mt-5" style={{ width: "18rem" }}>
+				<Card.Img variant="top" src={selected?.img} />
+				<Card.Body>
+					<Card.Title>{selected?.name}</Card.Title>
+					<Card.Text>
+						{" "}
+						<h5 className="text-primary">
+							What is {selected?.name}?
+						</h5> <br /> {selected?.description}
+					</Card.Text>
+					<Card.Text>Solution :{selected?.solution}</Card.Text>
+				</Card.Body>
+			</Card>
 		</div>
 	);
 };
